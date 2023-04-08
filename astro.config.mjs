c861c3786674cab79ed5a36959dfac693ca01af4
Brandon M. Scrushy <brandonscrushy@gmail.com>
2023-04-08 13:59:04 +0000

    this template is garbage. i'm done.

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,9 @@ import image from "@astrojs/image";
 import compress from "astro-compress";
 import partytown from "@astrojs/partytown";
 import sitemap from "@astrojs/sitemap";
+// import mdx from "@astrojs/mdx";
 
-import mdx from "@astrojs/mdx";
-
-// https://astro.build/config
 export default defineConfig({
-  site: 'https://unwrapped.design',
-  integrations: [tailwind(), image(), compress(), partytown(), sitemap(), mdx()]
+  site: 'https://essentialsounds.shop',
+  integrations: [tailwind(), image(), compress(), partytown(), sitemap()]
 });
