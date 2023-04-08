897b1be7f1c7f44b1fef47b4188e5af563476531
Brandon M. Scrushy <brandonscrushy@gmail.com>
2023-04-08 09:18:45 +0000

    finish adding mdx

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,11 @@ import image from "@astrojs/image";
 import compress from "astro-compress";
 import partytown from "@astrojs/partytown";
 import sitemap from "@astrojs/sitemap";
+
+import mdx from "@astrojs/mdx";
+
 // https://astro.build/config
 export default defineConfig({
-   site: 'https://unwrapped.design',
-  integrations: [tailwind(), image(), compress(), partytown(), sitemap()]
+  site: 'https://unwrapped.design',
+  integrations: [tailwind(), image(), compress(), partytown(), sitemap(), mdx()]
 });
